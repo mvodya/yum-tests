34a148beca74c87ffab39bffb3eb3bfe2ead725d
Mark Vodyanitskiy <mvodya@gmail.com>
2017-10-29 17:41:08 +1000

    Конструктор класса

--- space-jump/spacestar.h
+++ space-jump/spacestar.h
@@ -6,6 +6,7 @@ private:
 	float progress_; // Текущий прогресс для функции отображения
 	float angle; // Угол (для функции отображения, абстрактен)
 public:
+	SpaceStar() { x_ = 0, y_ = 0; progress_ = 0; angle = 1; }; // Конструктор
 	void draw() { drawPixel(x_, y_, 1.0f); } // Отрисовываем звезду
 	void update() { // Обновляем координаты звезды
 		progress_ += 0.01f;
