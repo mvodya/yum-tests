38c4be813706a654e613b8d6fadac6e9c1840a5c
Mark Vodyanitskiy <mvodya@gmail.com>
2017-10-29 10:56:04 +1000

    Основа для графики

--- space-jump/main.cpp
+++ space-jump/main.cpp
@@ -1,12 +1,69 @@
-/*
+﻿/*
 	Space jump
 */
 #include <iostream>
 
 #include <GL/freeglut.h>
 
+const int WINDOW_SIZE = 800; // Размер окна
+const int PIXEL_QUANTITY = 4; // Плотность пикселей
+// Вычисление количества пикселей (не общее, общее = sqrt(PIXEL_AMOUNT))
+const float PIXEL_AMOUNT = (float)WINDOW_SIZE / (float)PIXEL_QUANTITY;
+
+// Отрисовка одного пикселя
+void drawPixel(float x, float y, float color) {
+	float pixelSize = (1.0f / PIXEL_AMOUNT) * 2;
+	// Указываем цвет
+	glColor3f(color, color, color);
+	// Отрисовываем квадрат
+	glBegin(GL_QUADS);
+		glVertex2f(-1.0f + (pixelSize * x), 1.0f - pixelSize - (pixelSize * y)); // 0 1
+		glVertex2f(-1.0f + pixelSize + (pixelSize * x), 1.0f - pixelSize - (pixelSize * y)); // 1 1
+		glVertex2f(-1.0f + pixelSize + (pixelSize * x), 1.0f - (pixelSize * y)); // 1 0
+		glVertex2f(-1.0f + (pixelSize * x), 1.0f - (pixelSize * y)); // 0 0
+	glEnd();
+}
+
+// Обработка графики
+void graphicsLoop()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Чистим буфер
+	glutSwapBuffers();
+}
+
+// Таймер для обновления графики (раз в 1ms)
+void glutTimer(int value)
+{
+	glutPostRedisplay(); // Обновляем экран (тик)
+	glutTimerFunc(1, glutTimer, 1); // Снова запускаем таймер
+}
+
+// Обработка ввода с клавиатуры
+void keyboardHandler(unsigned char key, int x, int y)
+{
+	std::cout << "Key detected: " << key << std::endl; // Выводим ввод с клавиатуры (просто так)
+	if (key == 27) // Выход по ESC
+		exit(EXIT_SUCCESS);
+}
+
 int main(int argc, char **argv) {
 	std::cout << "Space jump test\n";
+
+	// Инициализация графики
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(WINDOW_SIZE, WINDOW_SIZE);
+	glutCreateWindow("Yum - Magic spiral test");
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_BLEND);
+
+	glutDisplayFunc(graphicsLoop);
+	glutKeyboardFunc(keyboardHandler);
+	glutTimerFunc(1, glutTimer, 1);
+
+	// Запускаем циклы
+	glutMainLoop();
 
 	return 0;
 }
